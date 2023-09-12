8bf7e2cf01552ba54c1192f5b641451b7aef759f
simonebilleri <simonebilleri2@gmail.com>
2023-09-12 22:24:23 +0200

    1

---
+++ _app/immutable/chunks/paths.c06c80d4.js
@@ -0,0 +1,1 @@
+var s;const i=((s=globalThis.__sveltekit_r6hiz4)==null?void 0:s.base)??"/simonebilleri-site";var e;const t=((e=globalThis.__sveltekit_r6hiz4)==null?void 0:e.assets)??i;export{t as a,i as b};
